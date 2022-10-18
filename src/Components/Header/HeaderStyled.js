409d21d7818892da38dc1bf6562baf86f131cce1
BrunoMonteiro25 <bruno.monteiro.silva25@gmail.com>
2022-10-18 13:02:20 -0300

    Menu fixo e lista de filmes

--- src/Components/Header/HeaderStyled.js
+++ src/Components/Header/HeaderStyled.js
@@ -1,5 +1,16 @@
 import styled from 'styled-components'
 
+export const HeaderMenu = styled.header`
+  width: 100%;
+  top: 0;
+  position: fixed;
+
+  &.ativo {
+    background-color: #111;
+    transition: 0.2s;
+  }
+`
+
 export const NavContainer = styled.nav`
   max-width: 100rem;
   margin: 0 auto;
@@ -8,18 +19,30 @@ export const NavContainer = styled.nav`
   align-items: center;
   justify-content: space-between;
   flex-wrap: wrap;
-  z-index: 100;
+
+  @media (max-width: 991px) {
+    padding: 20px 50px;
+    flex-direction: column;
+  }
 `
 
 export const Menu = styled.div`
   display: flex;
   gap: 3rem;
+  justify-content: center;
+  align-items: center;
+
+  @media (max-width: 991px) {
+    flex-direction: column;
+    gap: 1rem;
+  }
 
   a {
     text-decoration: none;
     font-weight: 500;
     letter-spacing: 1px;
     transition: 0.3s;
+    font-size: 1rem;
   }
 
   a:hover {
@@ -29,6 +52,7 @@ export const Menu = styled.div`
   img {
     width: 92.5px;
     height: 25px;
+    margin-top: 10px;
   }
 
   ul {
@@ -45,15 +69,20 @@ export const Search = styled.div`
   height: 40px;
   position: relative;
 
+  @media (max-width: 991px) {
+    margin-top: 1.5rem;
+  }
+
   input {
     width: 100%;
     height: 100%;
-    padding: 0 10px 0 45px;
+    padding-left: 45px;
     color: #fff;
     background: transparent;
     border: 1px solid rgba(255, 255, 255, 0.5);
     outline: none;
     border-radius: 4px;
+    backdrop-filter: blur(10px);
   }
 
   input::placeholder {
@@ -61,8 +90,7 @@ export const Search = styled.div`
   }
 
   .icone {
-    width: 17px;
-    height: 17px;
+    font-size: 1rem;
     position: absolute;
     top: 50%;
     left: 10px;
--- src/Components/Home/HomeStyled.js
+++ src/Components/Home/HomeStyled.js
@@ -3,4 +3,63 @@ import styled from 'styled-components'
 export const Container = styled.div`
   max-width: 80rem;
   margin: 0 auto;
+
+  h1 {
+    text-align: center;
+    margin: 4rem 0;
+  }
+`
+
+export const MovieList = styled.ul`
+  list-style: none;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  column-gap: 2rem;
+  row-gap: 4rem;
+`
+
+export const Movie = styled.li`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  img {
+    width: 180px;
+    border-radius: 1rem;
+    margin-bottom: 2rem;
+  }
+
+  span {
+    font-weight: bold;
+    font-size: 120%;
+    text-align: center;
+    line-height: 1.2;
+  }
+
+  a {
+    transition: all 0.3s;
+  }
+
+  a:hover {
+    transform: scale(1.1);
+  }
+`
+
+export const Banner = styled.div`
+  position: relative;
+  width: 100%;
+  min-height: 100vh;
+  padding: 0 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
 `
