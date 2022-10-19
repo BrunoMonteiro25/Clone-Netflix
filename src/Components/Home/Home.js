283d812e33fbb40163d25239c058a5d709c5c953
Bruno Monteiro <bno_mts1@hotmail.com>
2022-10-19 19:35:43 -0300

    Banner atualizado

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import Header from '../Header/Header'
-import { APIKey } from '../../Config/api'
+import { APIKey, APIBase } from '../../Config/api'
 import {
   Container,
   Movie,
@@ -19,9 +19,7 @@ function Home() {
   const image_path = 'https://image.tmdb.org/t/p/w500'
 
   useEffect(() => {
-    fetch(
-      `https://api.themoviedb.org/3/movie/popular?api_key=${APIKey}&language=pt-BR&page=1`,
-    )
+    fetch(`${APIBase}/movie/popular?api_key=${APIKey}&language=pt-BR&page=1`)
       .then((response) => response.json())
       .then((data) => setMovies(data.results))
   }, [])
@@ -34,11 +32,12 @@ function Home() {
       <Header />
       <Details>
         <div>
-          <h3>Mulan</h3>
+          <h3>Vingadores: Ultimato</h3>
           <p>
-            Para salvar seu pai doente de servir no Exército Imperial, uma jovem
-            destemida se disfarça de homem para lutar contra os invasores do
-            norte da China.
+            Após Thanos eliminar metade das criaturas vivas, os Vingadores têm
+            de lidar com a perda de amigos e entes queridos. Com Tony Stark
+            vagando perdido no espaço sem água e comida, Steve Rogers e Natasha
+            Romanov lideram a resistência contra o titã louco.
           </p>
           <button className="button">
             <BsFillPlayFill className="icone" />
@@ -48,7 +47,7 @@ function Home() {
       </Details>
 
       <Container>
-        <h1>Populares na Netflix</h1>
+        <h2>Populares na Netflix</h2>
         <MovieList>
           {movies.map((movie) => {
             return (
--- src/Config/api.js
+++ src/Config/api.js
@@ -1,1 +1,2 @@
 export const APIKey = '5b5c4bcb2cda8e6cc5c45d08e8f31ec1'
+export const APIBase = 'https://api.themoviedb.org/3'
